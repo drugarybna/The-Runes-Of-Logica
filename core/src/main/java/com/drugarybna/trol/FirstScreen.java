9371a776cc99da530e0871853f5c290517ab1f11
drugarybna <appelsene.design@gmail.com>
2025-06-05 19:54:53 +0300

    Construction of basic main menu

--- core/src/main/java/com/drugarybna/trol/FirstScreen.java
+++ core/src/main/java/com/drugarybna/trol/FirstScreen.java
@@ -1,25 +1,73 @@
 package com.drugarybna.trol;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.ray3k.stripe.FreeTypeSkin;
+
+import static com.badlogic.gdx.Gdx.gl;
 
 public class FirstScreen implements Screen {
+
+    private Stage stage;
+    private FreeTypeSkin skin;
+
     @Override
     public void show() {
-        // Prepare your screen here.
+
+        skin = new FreeTypeSkin(Gdx.files.internal("skin/logica_skin.json"));
+
+        stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        Gdx.input.setInputProcessor(stage);
+
+        Table mainTable = new Table();
+        mainTable.setFillParent(true);
+        mainTable.right();
+        mainTable.padRight(100);
+        stage.addActor(mainTable);
+
+        ImageButton title = new ImageButton(skin, "default");
+        TextButton buttonPlay = new TextButton("Play", skin);
+        TextButton buttonExit = new TextButton("Exit", skin);
+        buttonExit.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        mainTable.add(title).right().width(360).height(194);
+        mainTable.row().padTop(50);
+        mainTable.add(buttonPlay).right().width(130).height(50);
+        mainTable.row().padTop(20);
+        mainTable.add(buttonExit).right().width(130).height(50);
+
+        Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+
     }
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+
+
+        Gdx.gl.glClearColor(0.012f, 0.020f, 0.078f, 1f);
+        gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.getViewport().apply();
+        stage.act();
+        stage.draw();
+
     }
 
     @Override
     public void resize(int width, int height) {
-        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
-        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
-        if(width <= 0 || height <= 0) return;
 
-        // Resize your screen here. The parameters represent the new window size.
+        stage.getViewport().update(width, height, true);
+
     }
 
     @Override
@@ -39,6 +87,8 @@ public class FirstScreen implements Screen {
 
     @Override
     public void dispose() {
-        // Destroy screen's assets here.
+
+        stage.dispose();
+
     }
 }
